d184a11e4982147619087b1dc5afb8369d5d2ddd
Rodion <i.rodion.r@gmail.com>
2021-08-27 23:57:29 +0200

    Add basic CNN model

--- magnetometer/data_labelling.py
+++ magnetometer/data_labelling.py
@@ -98,71 +98,6 @@ df_train_labelled_description.to_excel('df_train_labelled_description.xlsx')
 # df_train_minute_labelled_description.to_excel(
 #    'df_train_minute_labelled_description.xlsx')
 
-# %% select features
-
-
-def select_features(df, *features):
-
-    drop_col = ['DATE', 'X_MSO', 'Y_MSO', 'Z_MSO', 'BX_MSO', 'BY_MSO', 'BZ_MSO', 'DBX_MSO', 'DBY_MSO', 'DBZ_MSO', 'RHO_DIPOLE', 'PHI_DIPOLE', 'THETA_DIPOLE',
-                'BABS_DIPOLE', 'BX_DIPOLE', 'BY_DIPOLE', 'BZ_DIPOLE', 'RHO', 'RXY', 'X', 'Y', 'Z', 'VX', 'VY', 'VZ', 'VABS', 'D', 'COSALPHA', 'EXTREMA']
-
-    for feature in features:
-        if feature in drop_col:
-            drop_col.remove(feature)
-
-    if 'MSO' in features:
-        drop_col.remove('X_MSO')
-        drop_col.remove('Y_MSO')
-        drop_col.remove('Z_MSO')
-
-    if 'B_MSO' in features:
-        drop_col.remove('BX_MSO')
-        drop_col.remove('BY_MSO')
-        drop_col.remove('BZ_MSO')
-
-    if 'DB_MSO' in features:
-        drop_col.remove('DBX_MSO')
-        drop_col.remove('DBY_MSO')
-        drop_col.remove('DBZ_MSO')
-
-    if 'B_DIPOLE' in features:
-        drop_col.remove('BX_DIPOLE')
-        drop_col.remove('BY_DIPOLE')
-        drop_col.remove('BZ_DIPOLE')
-
-    if 'SE' in features:
-        drop_col.remove('X')
-        drop_col.remove('Y')
-        drop_col.remove('Z')
-
-    if 'VSE' in features:
-        drop_col.remove('VX')
-        drop_col.remove('VY')
-        drop_col.remove('VZ')
-
-    # unused
-    if 'AB' in features:
-        drop_col.remove('X_AB')
-        drop_col.remove('Y_AB')
-        drop_col.remove('Z_AB')
-
-    # unused
-    if 'B_AB' in features:
-        drop_col.remove('BX_AB')
-        drop_col.remove('BY_AB')
-        drop_col.remove('BZ_AB')
-
-    # unused
-    if 'DB_AB' in features:
-        drop_col.remove('DBX_AB')
-        drop_col.remove('DBY_AB')
-        drop_col.remove('DBZ_AB')
-
-    return df.drop(drop_col, axis=1)
-
-
-df_train_selection = select_features(df_train, 'MSO', 'SE')
-
 # %% split data into chunks and shuffle them
 
 
@@ -184,6 +119,6 @@ def shuffle_df(df_list):
     return pd.concat(df_list)
 
 
-df_train_split = shuffle_df(split_df(df_train_selection, 10))
+df_train_split = shuffle_df(split_df(df_train, 10))
 
 # %%
---
+++ magnetometer/magnetometer_model.py
@@ -0,0 +1,85 @@
+# %% imports
+
+import glob
+
+import pandas as pd
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# %% load data
+
+
+def load_data(path):
+
+    files = glob.glob(path)
+    li = []
+
+    for filename in files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        li.append(df)
+
+    df = pd.concat(li, axis=0, ignore_index=True)
+
+    return df.dropna()
+
+
+df_train = load_data('../data/labelled_orbits/train/*.csv')
+df_test = load_data('../data/labelled_orbits/test/*.csv')
+
+
+# %% select features
+
+
+def select_features(df, features):
+
+    drop_col = ['Unnamed: 0', 'DATE', 'X_MSO', 'Y_MSO', 'Z_MSO', 'BX_MSO', 'BY_MSO', 'BZ_MSO', 'DBX_MSO', 'DBY_MSO', 'DBZ_MSO', 'RHO_DIPOLE', 'PHI_DIPOLE', 'THETA_DIPOLE',
+                'BABS_DIPOLE', 'BX_DIPOLE', 'BY_DIPOLE', 'BZ_DIPOLE', 'RHO', 'RXY', 'X', 'Y', 'Z', 'VX', 'VY', 'VZ', 'VABS', 'D', 'COSALPHA', 'EXTREMA', 'ORBIT']
+
+    for feature in features:
+        if feature in drop_col:
+            drop_col.remove(feature)
+
+    if 'INDEX' in features:
+        drop_col.remove('Unnamed: 0')
+
+    return df.drop(drop_col, axis=1)
+
+
+features = ['X', 'Y', 'Z', 'COSALPHA']
+df_train = select_features(df_train, features)
+df_test = select_features(df_test, features)
+
+# %% truncate data
+
+df_train = df_train[:10**(len(str(len(df_train)))-1)]
+df_test = df_test[:10**(len(str(len(df_test)))-1)]
+
+# %% split input and output data
+
+x_train = df_train.drop(['LABEL'], axis=1).values.reshape(
+    100, -1, len(features))
+x_test = df_test.drop(['LABEL'], axis=1).values.reshape(100, -1, len(features))
+y_train = df_train[['LABEL']].values.reshape(100, -1, 1)
+y_test = df_test[['LABEL']].values.reshape(100, -1, 1)
+
+# %% set up the model
+
+model = keras.Sequential()
+model.add(layers.Conv1D(16, 3, padding='same', activation='relu',
+          batch_input_shape=x_train[1:].shape))
+model.add(layers.MaxPooling1D(2))
+model.add(layers.Conv1D(32, 3, activation='relu'))
+model.add(layers.MaxPooling1D(2))
+model.add(layers.Conv1D(64, 3, activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(5, activation='softmax'))
+
+model.compile(loss=keras.losses.SparseCategoricalCrossentropy(),
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, batch_size=64, epochs=5, verbose=2)
+_, acc = model.evaluate(x_test, y_test, batch_size=64, verbose=2)
+
+# %%
