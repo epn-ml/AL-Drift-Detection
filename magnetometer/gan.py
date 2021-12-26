00e031aca9d255e2b2eb3acf497110e137f7432d
Rodion <i.rodion.r@gmail.com>
2021-12-26 19:36:49 +0100

    Fix logging

--- magnetometer/gan.py
+++ magnetometer/gan.py
@@ -152,11 +152,10 @@ def collate_generator(batch):
     global seq_len
     # Stack each tensor variable
     feature_length = int(len(batch[0]) / (seq_len + 1))
+    print_('---------------------------------------')
     print_(f'seq_len = {seq_len}, feature_length = {feature_length}')
-    print_(f'type(batch) = {type(batch)}')
-    print_(f'type(batch[0]) = {type(batch[0])}')
     print_(f'len(batch) = {len(batch)}')
-    print_(f'len(batch[0]) = {len(batch[0])}')
+    print_(f'batch[0].shape = {batch[0].shape}')
     # The last feature length corresponds to the feature we want to predict and
     # the last value is the label of the drift class
     x = torch.stack([torch.Tensor(np.reshape(x[:-feature_length-1], newshape=(seq_len, feature_length)))
