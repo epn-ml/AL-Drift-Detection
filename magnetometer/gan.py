0acd7b4b5a7917ac4db7ece4f93857e4f6f3dd7d
Rodion <i.rodion.r@gmail.com>
2021-12-27 12:59:49 +0100

    Fix feature length

--- magnetometer/gan.py
+++ magnetometer/gan.py
@@ -151,10 +151,10 @@ def collate_generator(batch):
     """
     global seq_len
     # Stack each tensor variable
-    feature_length = int(len(batch[0]) / (seq_len + 1)) - 1
+    feature_length = int(len(batch[0]) / (seq_len + 1))
     # The last feature length corresponds to the feature we want to predict and
     # the last value is the label of the drift class
-    x = torch.stack([torch.Tensor(np.reshape(x[:-feature_length-1], newshape=(seq_len, feature_length)))
+    x = torch.stack([torch.Tensor(np.reshape(x[:-feature_length-1], newshape=(seq_len, feature_length - 1)))
                      for x in batch])
     y = torch.stack([torch.tensor(x[-feature_length-1:-1]) for x in batch])
     labels = torch.stack([torch.tensor(x[-1]) for x in batch])
