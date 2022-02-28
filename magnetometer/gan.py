b3e000181fe26b61ee131954d9886e90d0ea943f
Rodion <i.rodion.r@gmail.com>
2022-02-28 16:34:22 +0100

    Debug create_training_dataset

--- magnetometer/gan.py
+++ magnetometer/gan.py
@@ -630,9 +630,11 @@ def detect_drifts(features, orbits, dates, device, epochs=100, steps_generator=1
 
         # print_(f'training dataset indices = {drift_indices}')
         # print_(f'training dataset labels  = {drift_labels} + {temp_label}')
+        t0 = time.perf_counter()
         training_dataset = create_training_dataset(dataset=features,
                                                    indices=drift_indices,
                                                    drift_labels=drift_labels+temp_label)
+        print_(f'created training dataset {time.perf_counter() - t0}')
         # print_(f'len(training_dataset) = {len(training_dataset)}')
 
         generator, discriminator = train_gan(features=training_dataset, device=device,
