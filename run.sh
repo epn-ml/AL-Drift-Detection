5e802c08c4650ee1ac7447dd2ea0e05d8e0adcee
Rodion Ishmukhametov <ishmuk01@ads.uni-passau.de>
2022-04-26 16:44:31 +0000

    Update run script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in {1..4}; do
-    dir = 'logs/$(date "+%Y/%m/%d %H:%M:%S")'
-    echo $dir
-    echo '$i/4'
-    python gan.py $dir $i
+for i in {1..1}; do
+    log="logs/$(date '+%Y-%m-%d_%H-%M-%S')_set$i"
+    echo $log
+    echo "$i/4"
+    python gan.py $log $i
 done
