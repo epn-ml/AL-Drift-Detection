1492c32387b6db03a87aeab1c6d67ad881061d0b
Rodion <i.rodion.r@gmail.com>
2022-05-15 18:25:03 +0200

    Fix logs

--- gan.py
+++ gan.py
@@ -615,8 +615,8 @@ def detect_drifts(df, device, epochs=100, steps_generator=100, equalize=True, te
         #     f'assigning drift {next_label} to orbits {new_orbits[0]} - {new_orbits[-1]}, generator_label {generator_label}')
         # print_(
         #     f'indices = {(orbits_idx[cur_orbit][0], orbits_idx[end_orbit-1][1])}')
-        print_(f'orbits {new_orbits[0]} - {new_orbits[-1]}\t({end_orbit-cur_orbit}, {end_orbit}/{len(orbit_numbers)})\t-- \
-                drift {next_label}\t({max_idx_prev_saved} -> {max_idx} [{idx_saved}])')
+        print_(
+            f'orbits {new_orbits[0]} - {new_orbits[-1]} ({end_orbit-cur_orbit}, {end_orbit}/{len(orbit_numbers)}) -- drift {next_label}\t({max_idx_prev_saved} -> {max_idx} [{idx_saved}])')
 
         drift_indices.append(
             (orbits_idx[cur_orbit][0], orbits_idx[end_orbit-1][1]))
