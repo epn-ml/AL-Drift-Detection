39273246bbcddc6eef5615eab82a4034327d9442
Rodion <i.rodion.r@gmail.com>
2022-05-17 12:38:36 +0200

    Fix order of drifts

--- gan.py
+++ gan.py
@@ -608,20 +608,6 @@ def detect_drifts(df, device, epochs=100, steps_generator=100, equalize=True, te
             #     print_(
             #         f'detected drift at the start of orbit {orbit_numbers[cur_orbit]} - {orbits_idx[cur_orbit]}')
 
-        new_orbits = orbit_numbers[cur_orbit:end_orbit]
-        new_drift_orbits = dict(zip(new_orbits, [next_label]*len(new_orbits)))
-        drift_orbits = {**drift_orbits, **new_drift_orbits}
-        # print_(
-        #     f'assigning drift {next_label} to orbits {new_orbits[0]} - {new_orbits[-1]}, generator_label {generator_label}')
-        # print_(
-        #     f'indices = {(orbits_idx[cur_orbit][0], orbits_idx[end_orbit-1][1])}')
-        print_(
-            f'{end_orbit}/{len(orbit_numbers)} orbits {new_orbits[0]} - {new_orbits[-1]} ({end_orbit-cur_orbit}) -- drift {next_label} (max_idx = {max_idx})')
-
-        drift_indices.append(
-            (orbits_idx[cur_orbit][0], orbits_idx[end_orbit-1][1]))
-        drift_labels.append(next_label)
-
         max_idx = max_idx[0]
         if max_idx != generator_label:
             # Increase the max_idx by 1 if it is above the previous drift
@@ -635,6 +621,7 @@ def detect_drifts(df, device, epochs=100, steps_generator=100, equalize=True, te
             temp_label = [max_idx]
             discriminator.reset_top_layer()
             discriminator = discriminator.to(device)
+            next_label = max_idx
             print_(
                 f'Previous drift {max_idx} occured at {index} (orbits {orbit_numbers[cur_orbit]} - {orbit_numbers[end_orbit-1]})')
 
@@ -647,6 +634,20 @@ def detect_drifts(df, device, epochs=100, steps_generator=100, equalize=True, te
             discriminator.update()
             discriminator = discriminator.to(device)
             generator_label += 1
+
+        new_orbits = orbit_numbers[cur_orbit:end_orbit]
+        new_drift_orbits = dict(zip(new_orbits, [next_label]*len(new_orbits)))
+        drift_orbits = {**drift_orbits, **new_drift_orbits}
+        # print_(
+        #     f'assigning drift {next_label} to orbits {new_orbits[0]} - {new_orbits[-1]}, generator_label {generator_label}')
+        # print_(
+        #     f'indices = {(orbits_idx[cur_orbit][0], orbits_idx[end_orbit-1][1])}')
+        print_(
+            f'{end_orbit}/{len(orbit_numbers)} orbits {new_orbits[0]} - {new_orbits[-1]} ({end_orbit-cur_orbit}) -- drift {next_label} (max_idx = {max_idx})')
+
+        drift_indices.append(
+            (orbits_idx[cur_orbit][0], orbits_idx[end_orbit-1][1]))
+        drift_labels.append(next_label)
 
         generator = Generator(
             inp=features.shape[1], out=features.shape[1], sequence_length=sequence_length)
