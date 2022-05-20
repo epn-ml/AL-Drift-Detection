13c34612937a398ce8c03a95ab8d16ab41efad5d
Rodion <i.rodion.r@gmail.com>
2022-05-20 14:21:07 +0200

    Fix print

--- cnn.py
+++ cnn.py
@@ -32,28 +32,29 @@ def print_(print_str, with_date=True):
     global fptr
     print_f(fptr, print_str, with_date)
     if with_date:
-        print(print_str)
-    else:
         print(f'{str(datetime.now())}: {print_str}')
+    else:
+        print(print_str)
 
 
-# Custom metric
+# Custom metrics
+def recall_m(y_true, y_pred):
+    TP = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    Positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+
+    recall = TP / (Positives + K.epsilon())
+    return recall
+
+
+def precision_m(y_true, y_pred):
+    TP = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    Pred_Positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+
+    precision = TP / (Pred_Positives + K.epsilon())
+    return precision
+
+
 def f1(y_true, y_pred):
-
-    def recall_m(y_true, y_pred):
-        TP = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        Positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-
-        recall = TP / (Positives + K.epsilon())
-        return recall
-
-    def precision_m(y_true, y_pred):
-        TP = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        Pred_Positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-
-        precision = TP / (Pred_Positives + K.epsilon())
-        return precision
-
     precision, recall = precision_m(y_true, y_pred), recall_m(y_true, y_pred)
 
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
@@ -72,7 +73,7 @@ def cnn(shape):
 
     model.compile(loss=keras.losses.SparseCategoricalCrossentropy(),
                   optimizer=keras.optimizers.Adam(learning_rate=0.001),
-                  metrics=['accuracy', metrics.sparse_categorical_accuracy, f1])
+                  metrics=['accuracy', metrics.sparse_categorical_accuracy, f1, recall_m, precision_m])
 
     return model
 
