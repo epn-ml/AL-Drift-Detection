70cb0aeb21c95e040e0458835f95bf7a675d6070
Rodion <i.rodion.r@gmail.com>
2022-07-02 10:49:27 +0200

    Update run script 26-50

--- run-cnn.sh
+++ run-cnn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 log="logs/cnn_$(date '+%Y-%m-%d_%H-%M-%S')"
 echo $log
-for i in {65..75}; do
-    echo "max_orbits $i/25"
-    python cnn.py $log 7 $1 $i
+for i in {26..50}; do
+    echo "max_orbits $i/50"
+    python cnn.py $log 6 $1 $i
 done
